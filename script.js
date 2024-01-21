43fcb3e3de0ddea39996f87dbbd083e7ac41efe3
wbu <willy.burgos@witbe.net>
2024-01-21 04:20:35 +0100

    add colors for answers + continue button

--- script.js
+++ script.js
@@ -29,11 +29,57 @@ totalRoundsElem.innerHTML = rounds;
 
 const scoreElem = document.getElementById("score");
 
+// other
+const submitButton = document.getElementById("submit-btn");
+let isBetweenRounds = false;
+
 // Functions
+function resetGame() {
+  score = 0;
+  currentRound = 1;
+  scoreElem.innerHTML = "";
+  currentRoundElem.innerHTML = currentRound;
+  isBetweenRounds = false;
+}
+
+function nextSlide() {
+  randomVid.currentTime = 0;
+  randomVid.style.width = "0";
+  randomIndex = Math.floor(Math.random() * vids.length);
+  randomVid = vids[randomIndex];
+  randomVid.style.width = "100%";
+  currentRound++;
+  if (currentRound > rounds) {
+    alert(`You scored ${score} out of ${rounds}!`);
+    resetGame();
+    return;
+  }
+  currentRoundElem.innerHTML = currentRound;
+}
+
 function handleSubmit(e) {
   e.preventDefault();
-
   const form = e.target;
+  const radioButtons = form.querySelectorAll('input[type="radio"]');
+  const labels = form.querySelectorAll(".answer-label");
+
+  if (isBetweenRounds) {
+    isBetweenRounds = false;
+    submitButton.innerHTML = "Check your answer";
+    submitButton.classList.remove("next-slide");
+    submitButton.classList.add("check-answer");
+    radioButtons.forEach((radioButton) => {
+      radioButton.disabled = false;
+      radioButton.style.opacity = "1";
+    });
+    labels.forEach((label) => {
+      label.style.color = "white";
+    });
+    nextSlide();
+    form.reset();
+    return;
+  }
+
   const formData = new FormData(form);
   const answer = parseInt(formData.get("frame"));
 
@@ -43,20 +89,18 @@ function handleSubmit(e) {
       scoreElem.innerHTML = `Answered correctly ${score} ${
         score > 1 ? "times" : "time"
       }!`;
+    } else {
+      document.getElementById(`answer-${answer}`).style.color = "red";
     }
-    currentRound++;
-    if (currentRound > rounds) {
-      alert(`You scored ${score} out of ${rounds}!`);
-      score = 0;
-      currentRound = 1;
-      scoreElem.innerHTML = "";
-    }
-    randomVid.currentTime = 0;
-    randomVid.style.width = "0";
-    randomIndex = Math.floor(Math.random() * vids.length);
-    randomVid = vids[randomIndex];
-    randomVid.style.width = "100%";
+    document.getElementById(`answer-${randomIndex}`).style.color = "green";
+    isBetweenRounds = true;
+    submitButton.innerHTML = "Next slide";
+    submitButton.classList.remove("check-answer");
+    submitButton.classList.add("next-slide");
+    radioButtons.forEach((radioButton) => {
+      radioButton.disabled = true;
+      radioButton.style.opacity = "0.3";
+    });
     form.reset();
-    currentRoundElem.innerHTML = currentRound;
   }
 }
